7d15a53c42e15698c9ad38aa8bb87a23cc346bf0
David Jay <davidgljay@gmail.com>
2016-01-11 01:01:35 -0500

    Updating data extraction lambda functions.

--- data_extraction_lambda/start.js
+++ data_extraction_lambda/start.js
@@ -2,6 +2,7 @@ var aws = require('aws-sdk');
 
 var ec2 = new aws.EC2({apiVersion: '2015-10-01'});
 var ecs = new aws.ECS();
+var dynamodb = new aws.DynamoDB({apiVersion: '2015-02-02'})
 
 module.exports.handler = function(event, context) {
     var instanceParams = {
@@ -11,21 +12,36 @@ module.exports.handler = function(event, context) {
     ec2.startInstances(instanceParams, function(err, data) {
         if (err) context.fail(err);
     })
-    ec2.waitFor('instanceRunning', instanceParams, function(err, data) {
-        if (err) context.fail(err);
-        else runTask(context);
+    
+    updateDynamo(function(err, data) {
+        if (err && err.code!="ValidationException") context.fail(err);
+        else ec2.waitFor('instanceRunning', instanceParams, function(err, data) {
+            if (err) context.fail(err);
+            else runTask(context);
+        });       
     });
 };
 
 var runTask = function(context) {
     var ContainerParams = {
-      taskDefinition: 'MayorsDB_Data',
+      taskDefinition: 'mayorpressdb',
       cluster: 'mayorsdb',
       count: 1
     };
     ecs.runTask(ContainerParams, function(err, data) {
       if (err) context.fail(err);
-      else if (data.failure) setInterval(runTask(), 250);
+      else if (data.failures) setTimeout(runTask(), 250);
       else context.succeed("Successfully started task\n" + JSON.stringify(data));
   });
+}
+
+var updateDynamo = function(callback) {
+    var dynamoUpdateParams = {
+      TableName: 'mayorsdb_articles',
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 1,
+        WriteCapacityUnits: 10
+      }
+    };
+    dynamodb.updateTable(dynamoUpdateParams, callback);
 }
--- data_extraction_lambda/stop.js
+++ data_extraction_lambda/stop.js
@@ -1,9 +1,13 @@
 var aws = require('aws-sdk');
 var ec2 = new aws.EC2({apiVersion: '2015-10-01'});
+var dynamodb = new aws.DynamoDB({apiVersion: '2015-02-02'});
 
 module.exports.handler = function(event, context) {
-    console.log(event);
-    if (event.default.AlarmName == "MayorsDB") {
+    console.log("Triggered with event: ");
+    console.log(event.Records[0].Sns.Message);
+    var message = JSON.parse(event.Records[0].Sns.Message)
+    if (message.AlarmName == "MayorsDB_Notif") {
+        console.log("Stopping EC2 instance");
         var instanceParams = {
           InstanceIds: [ 'i-eb0b645a' ],
           DryRun: false
@@ -14,8 +18,23 @@ module.exports.handler = function(event, context) {
             } else {
                 context.succeed(data);
             }
-        });            
+        });
+        updateDynamo();
     } else {
         context.succeed();
     }
 };
+
+var updateDynamo = function() {
+    var dynamoUpdateParams = {
+      TableName: 'mayorsdb_articles',
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 1,
+        WriteCapacityUnits: 1
+      }
+    };
+    dynamodb.updateTable(dynamoUpdateParams, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else     console.log(data);           // successful response
+    });
+}
